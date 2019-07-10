6ef5c2e3bc4184ca648cbb3f0a32db19ed3e7680
yangyangma <yangyangma2016@gmail.com>
2019-07-10 09:43:23 -0400

    gui - JOptionPane

---
+++ FIrstGUI/src/Bank/Main.java
@@ -0,0 +1,11 @@
+package Bank;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        JOptionPane.showMessageDialog(null,"Welcome to Java!", "My Application", JOptionPane.ERROR_MESSAGE);
+    }
+}
